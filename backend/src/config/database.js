dc6de57e5d222833eac3dcbf5ecb0d8291830e3f
Marilena Sales <marilena.sales@gmail.com>
2021-02-02 15:58:37 -0300

    refazer

--- backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-//importação mongoose
-const mongoose = require('mongoose');
-
-//definir url de conexão com o mongo
-const url = 'mongodb://localhost:27017/todo';
-mongoose.connect(url, { useNewUrlParser: true});
-
-module.exports = mongoose;
