5780a599943a0dc4f799f26a450af7078492e447
Marilena Sales <marilena.sales@gmail.com>
2021-02-07 16:50:32 -0300

    filtro de tarefa do dia

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,7 @@
 const TaskModel = require('../model/TaskModel');
+const {startOfDay, endOfDay} = require('date-fns');
+
+const current = new Date();
 
 class TaskController{
 // criar uma nova tarefa no BD
@@ -22,6 +25,95 @@ async update(req, res){
         return res.status(200).json(response);
     })
     .catch(error =>{
+        return res.status(500).json(error);
+    });
+}
+
+//rota para listar todas as tarefas
+async all(req, res){
+    //listar filtrando pelo macaddress
+    await TaskModel.find({macaddress: {'$in': req.body.macaddress}})
+    //organizar por data e hora
+    .sort('when')
+    .then(response =>{
+        return res.status(200).json(response);
+    })
+    .catch(error =>{
+        return res.status(500).json(error);
+    })
+
+}
+
+//exibir tarefa específica
+async show(req, res){
+    await TaskModel.findById(req.params.id)
+    .then(response =>{
+        if(response)
+        return res.status(200).json(response)
+        else
+        return res.status(404).json({error: 'tarefa não encontrada'});
+    })
+    .catch(error =>{
+        return res.status(500).json(error);
+    })
+}
+
+//deletar tarefa usando id
+async delete(req, res){
+    await TaskModel.deleteOne({'_id': req.params.id})
+    .then(response =>{
+        return res.status(200).json(response);
+    })
+    .catch(error =>{
+        return res.status(500).json(error);
+    })
+}
+
+//atualizar o status de uma tarefa
+async done(req, res){
+    await TaskModel.findByIdAndUpdate(
+        {'_id': req.params.id},
+        {'done': req.params.done},
+        {new: true})
+        .then(response =>{
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        })
+}
+
+//exibir tarefas atrasadas
+async late(req, res){
+    //procurar baseado na data,primeiro saber data e hora atual com a const current
+    await TaskModel
+    .find({
+        //data menor ou igual a current
+        'when': {'$lt': current},
+        'macaddress': {'$in': req.body.macaddress}
+    })
+    //organizar tarefas por data e hora
+    .sort('when')
+    .then(response =>{
+        return res.status(200).json(response);
+    })
+    .catch(error =>{
+        return res.status(500).json(error);
+    });
+}
+
+//filtrar tarefa do dia
+async today(req, res){
+    await TaskModel
+    .find({'macaddress': {'$in': req.body.macaddress},
+    //data sej maior ou igual ao inicio do dia corrente 
+    'when': {'$gte': startOfDay(current), '$lt': endOfDay(current)}
+    })
+    .sort('when')
+    .then(response =>{
+        return res.status(200).json(response);
+    })
+    .catch(error => {
         return res.status(500).json(error);
     });
 }
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -3,8 +3,29 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
+const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id',TaskValidation,  TaskController.update);
+
+//mostrar uma unica tarefa
+router.get('/:id', TaskController.show);
+
+//para deletar tarefas
+router.delete('/:id', TaskController.delete);
+
+//atualizar o status de uma tarefa
+router.put('/:id/:done', TaskController.done);
+
+
+
+//rota do listar
+router.get('/filter/all', MacaddressValidation, TaskController.all)
+
+//exibir tarefas atrasadas
+router.get('/filter/late', MacaddressValidation, TaskController.late);
+
+//exibir tarefas do dia
+router.get('/filter/today', MacaddressValidation, TaskController.today);
 
 module.exports = router;
